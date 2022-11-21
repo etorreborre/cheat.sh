c6be8a1ebe7432d989c0e824749736c4fa75d7e9
etorreborre <etorreborre@yahoo.com>
2022-11-21 16:38:35 +0100

    floxify

---
+++ pkgs/cheat-sh/default.nix
@@ -0,0 +1,34 @@
+# Replace "stdenv" with the namespace or name of your language's builder
+{ self,
+  stdenv,
+  lib,
+  fetchurl,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "cheat-sh";
+  version = "1.0.0";
+  MACOSX_DEPLOYMENT_TARGET = true;
+
+  sourceRoot = ".";
+
+  src = fetchurl {
+    url = "https://cht.sh/:cht.sh";
+    sha256 = "sha256-92ORC10l4+UqjcCj+kc5MBQo2sn9qFFeM8HrsMYsFGo=";
+    executable = true;
+  };
+
+  phases = [ "installPhase" ];
+
+  installPhase = ''
+    install -m755 -D $src $out/bin/ch.sh
+  '';
+
+  meta = with lib; {
+    homepage = "https://github.com/chubin/cheat.sh";
+    description = "cheat.sh executable";
+    platforms = platforms.darwin;
+    architectures = [ "x86" ];
+  };
+
+}
